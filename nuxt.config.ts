475ab0e1a28f0afe7b189cbd846df7a5e2ee6938
Faradzh <faradzh@staffbase.com>
2024-03-07 14:37:02 +0600

    chore: add date-fns

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  buildModules: [
+    // Simple usage
+    "@nuxtjs/date-fns",
+  ],
   build: {
     transpile: ["@vuepic/vue-datepicker"],
   },
